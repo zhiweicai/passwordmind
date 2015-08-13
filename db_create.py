2c54291c3e313785df615201da93c6f82822378c
zhiweicai <cai.zhiwei@gmail.com>
2015-08-13 07:36:00 -0400

    update database script

--- db_create.py
+++ db_create.py
@@ -1,8 +1,8 @@
 #!flask/bin/python
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
-from app import db
+from password import SQLALCHEMY_DATABASE_URI
+from password import SQLALCHEMY_MIGRATE_REPO
+from password import db
 import os.path
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
